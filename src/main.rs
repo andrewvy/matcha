56d86c7161504edd30e45932e353737812703554
Andrew Vy <andrew@andrewvy.com>
2018-01-14 00:12:13 -0800

    :construction: Convert public key into hashed public ID/address

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate rust_sodium;
+extern crate hex;
 
 mod wallet;
 
 fn main() {
     let address = wallet::create_address();
 
+    println!("Public ID: {}", address.public_id.to_hex());
     println!("Public Key: {:?}", address.public_key);
     println!("Private Key: {:?}", address.private_key);
     println!("Amount: {:?}", address.amount);
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,7 +1,27 @@
 use rust_sodium::crypto::sign;
+use rust_sodium::crypto::hash;
+use hex;
+
+/// Constant byte length of `PublicId`.
+pub const PUBLIC_ID_LEN: usize = 32;
+
+
+/// Constant bit length of `PublicId`.
+#[allow(dead_code)]
+pub const PUBLIC_ID_BITS: usize = PUBLIC_ID_LEN * 8;
+
+#[derive(Clone, Debug)]
+pub struct PublicId(pub [u8; PUBLIC_ID_LEN]);
+
+impl PublicId {
+    pub fn to_hex(&self) -> String {
+        hex::encode(self.0.to_vec())
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct Address {
+    pub public_id: PublicId,
     pub public_key: sign::PublicKey,
     pub private_key: sign::SecretKey,
     pub amount: i64
@@ -11,6 +31,7 @@ pub fn create_address() -> Address {
     let (pk, sk) = sign::gen_keypair();
 
     Address {
+        public_id: PublicId(hash::sha256::hash(&pk[..]).0),
         public_key: pk,
         private_key: sk,
         amount: 0
