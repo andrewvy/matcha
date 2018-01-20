259021d21ac42c9ad6b0eeed9fe4888ea92413da
Andrew Vy <andrew@andrewvy.com>
2018-01-20 09:53:37 -0800

    :recycle: Dedupe connection handler

--- src/node.rs
+++ src/node.rs
@@ -3,6 +3,7 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::net::SocketAddr;
+
 use futures::sync::mpsc;
 use futures::{Future, Sink, Stream};
 
@@ -14,7 +15,7 @@ use failure::Error;
 use codec::MessageCodec;
 use matcha_pb::{self, Message, Request, Response};
 
-type PeerChannel = mpsc::UnboundedSender<Message>;
+type PeerTx = mpsc::UnboundedSender<Message>;
 
 type RefNodeData = Rc<RefCell<NodeData>>;
 
@@ -49,100 +50,54 @@ fn _debugs<S: Stream<Item = (), Error = Error>>(_: S) {}
 
 fn connect_to_peer(node_data: RefNodeData, handle: Handle, peer_addr: SocketAddr) -> Box<Future<Item = (), Error = Error>> {
     let client = TcpStream::connect(&peer_addr, &handle).and_then(move |conn| {
-        let (sink, stream) = conn.framed(MessageCodec).split();
-        let (peer_tx, peer_rx) = mpsc::unbounded::<Message>();
-        let (shutdown_tx, shutdown_rx) = mpsc::unbounded::<bool>();
-        let inner_handle = handle.clone();
-        let inner_node_data = node_data.clone();
-
-        add_peer(node_data.clone(), peer_addr, shutdown_tx.clone());
-
-        let mut peer = matcha_pb::Peer::new();
-        peer.set_addr(node_data.borrow().addr.to_string());
-
-        let mut peer_list = matcha_pb::PeerList::new();
-        peer_list.mut_peers().push(peer);
-
-        let mut peer_list_request = matcha_pb::PeerListRequest::new();
-        peer_list_request.set_peer_list(peer_list);
-
-        let mut request = Request::new();
-        request.set_field_type(matcha_pb::Request_Type::PEER_LIST_REQUEST);
-        request.set_peer_list_request(peer_list_request);
-
-        let mut message = Message::new();
-        message.set_field_type(matcha_pb::Message_Type::REQUEST);
-        message.set_request(request);
-
-        peer_tx.clone().unbounded_send(message).expect("could not send peerlist");
-
-        // Future<(), ()>
-        let reader = stream
-            .for_each(move |msg| handle_message(node_data.clone(), peer_addr, msg, inner_handle.clone(), peer_tx.clone()))
-            .map(move |_: ()| {
-                remove_peer(inner_node_data.clone(), peer_addr);
-                ()
-            })
-            .map_err(|_: Error| ());
-
-        // Stream<Message, Error>
-        let rx_stream = peer_rx.map_err(|_| format_err!("Error in sending!"));
-
-        // Stream<Message, Error> -> Future<(), ()>
-        let writer = sink.send_all(rx_stream).map(|_| ()).map_err(|_| ());
-
-        // Stream<SocketAddr>  -> Future((), ())
-        let shutdown_stream = shutdown_rx.map(|_| ()).map_err(|_| ()).into_future();
-        
-        handle.spawn(reader.select2(shutdown_stream).then(|_| Ok(())));
-        handle.spawn(writer);
-
+        handle_connection(conn, node_data.clone(), handle.clone(), peer_addr);
         Ok(())
     }).map(|_| ()).map_err(|_| format_err!("Error!"));
 
     Box::new(client)
 }
 
-fn serve(node_data: RefNodeData, handle: Handle, addr: &SocketAddr) -> Box<Future<Item = (), Error = io::Error>> {
-    let listener = TcpListener::bind(addr, &handle).unwrap();
+fn handle_connection(conn: TcpStream, node_data: RefNodeData, handle: Handle, addr: SocketAddr) {
+    let (sink, stream) = conn.framed(MessageCodec).split();
+    let (peer_tx, peer_rx) = mpsc::unbounded::<Message>();
+    let (shutdown_tx, shutdown_rx) = mpsc::unbounded::<bool>();
+    let inner_handle = handle.clone();
+    let inner_node_data = node_data.clone();
 
-    println!("Listening on: {}", addr);
+    add_peer(node_data.clone(), addr, shutdown_tx.clone());
+
+    // Future<(), ()>
+    let reader = stream
+        .for_each(move |msg| handle_message(node_data.clone(), addr, msg, inner_handle.clone(), peer_tx.clone()))
+        .map(move |_: ()| {
+            remove_peer(inner_node_data.clone(), addr);
+            ()
+        })
+        .map_err(|_: Error| ());
+
+    // Stream<Message, Error>
+    let rx_stream = peer_rx.map_err(|_| format_err!("Error in sending!"));
+
+    // Stream<Message, Error> -> Future<(), ()>
+    let writer = sink.send_all(rx_stream).map(|_| ()).map_err(|_| ());
+
+    // Stream<SocketAddr>  -> Future((), ())
+    let shutdown_stream = shutdown_rx.map(|_| ()).map_err(|_| ()).into_future();
+
+    handle.spawn(reader.select2(shutdown_stream).then(|_| Ok(())));
+    handle.spawn(writer);
+}
+
+fn serve(node_data: RefNodeData, handle: Handle) -> Box<Future<Item = (), Error = io::Error>> {
+    let listener = TcpListener::bind(&node_data.borrow().addr, &handle).unwrap();
+
+    println!("Listening on: {}", &node_data.borrow().addr);
 
     let server =
         listener
             .incoming()
             .for_each(move |(conn, peer_addr)| {
-                let (sink, stream) = conn.framed(MessageCodec).split();
-                let (peer_tx, peer_rx) = mpsc::unbounded::<Message>();
-                let (shutdown_tx, shutdown_rx) = mpsc::unbounded::<bool>();
-                let inner_handle = handle.clone();
-
-                let handler_node_data = node_data.clone();
-                let inner_node_data = node_data.clone();
-
-                add_peer(node_data.clone(), peer_addr, shutdown_tx.clone());
-
-                // Future<(), ()>
-                let reader = stream
-                    .for_each(move |msg| handle_message(handler_node_data.clone(), peer_addr, msg, inner_handle.clone(), peer_tx.clone()))
-                    .map(move |_: ()| {
-                        remove_peer(inner_node_data.clone(), peer_addr);
-                        ()
-                    })
-                    .map_err(|_: Error| ());
-
-                // Stream<Message, Error>
-                let rx_stream = peer_rx.map_err(|_| format_err!("Error in sending!"));
-
-                // Stream<Message, Error> -> Future<(), ()>
-                let writer = sink.send_all(rx_stream).map(|_| ()).map_err(|_| ());
-
-                // Stream<SocketAddr>  -> Future((), ())
-                let shutdown_stream = shutdown_rx.map(|_| ()).map_err(|_| ()).into_future();
-                
-                handle.spawn(reader.select2(shutdown_stream).then(|_| Ok(())));
-                handle.spawn(writer);
-
+                handle_connection(conn, node_data.clone(), handle.clone(), peer_addr);
                 Ok(())
             });
 
@@ -163,14 +118,14 @@ fn is_already_connected_to_peer(node_data: RefNodeData, peer_addr: SocketAddr) -
     node_data.borrow().peers.contains_key(&peer_addr)
 }
 
-fn handle_message(node_data: RefNodeData, _peer_addr: SocketAddr, message: Message, handle: Handle, peer_tx: PeerChannel) -> Result<(), Error> {
+fn handle_message(node_data: RefNodeData, _peer_addr: SocketAddr, message: Message, handle: Handle, peer_tx: PeerTx) -> Result<(), Error> {
     match message.get_field_type() {
         matcha_pb::Message_Type::REQUEST => handle_request(node_data, message.get_request(), peer_tx, handle),
         matcha_pb::Message_Type::RESPONSE => handle_response(node_data, message.get_response(), peer_tx, handle),
     }
 }
 
-fn handle_request(node_data: RefNodeData, request: &Request, _peer_tx: PeerChannel, handle: Handle) -> Result<(), Error> {
+fn handle_request(node_data: RefNodeData, request: &Request, _peer_tx: PeerTx, handle: Handle) -> Result<(), Error> {
     match request.get_field_type() {
         matcha_pb::Request_Type::PING_REQUEST => {
             // node_data.borrow_mut().handle_ping_request(request.get_ping_request(), channel)
@@ -199,7 +154,7 @@ fn handle_request(node_data: RefNodeData, request: &Request, _peer_tx: PeerChann
     }
 }
 
-fn handle_response(_node_data: Rc<RefCell<NodeData>>, response: &Response, _peer_tx: PeerChannel, _handle: Handle) -> Result<(), Error> {
+fn handle_response(_node_data: Rc<RefCell<NodeData>>, response: &Response, _peer_tx: PeerTx, _handle: Handle) -> Result<(), Error> {
     match response.get_field_type() {
         matcha_pb::Response_Type::PING_RESPONSE => {
             // node_data.borrow_mut().handle_ping_response(response.get_ping_response(), channel)
@@ -232,7 +187,7 @@ pub fn boot(server_addr: String, bootstrap_nodes: Vec<&str>) {
     let handle = core.handle();
     let node = Node::new(parsed_server_addr);
 
-    let server = serve(node.data.clone(), handle.clone(), &parsed_server_addr);
+    let server = serve(node.data.clone(), handle.clone());
     let inner_handle = handle.clone();
 
     for addr in formatted_bootstrap_nodes {
